db3e3c1ed49f7d524f09ce674c34908e2a334663
Hesham Massoud <hesham.massoud@commercetools.de>
2018-12-11 18:57:59 +0100

    #1: Add gradle build setup.

---
+++ src/main/java/com/commercetools/sync/Main.java
@@ -0,0 +1,13 @@
+package com.commercetools.sync;
+
+
+public class Main {
+    /**
+     * Application entry point.
+     *
+     * @param args all args
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello Project Sync!");
+    }
+}
