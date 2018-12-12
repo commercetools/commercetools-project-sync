7b347acd2e81b3f0d6cdefca81d08226077a8751
Hesham Massoud <hesham.massoud@commercetools.de>
2018-12-12 13:51:42 +0100

    #1: Change SOUT to Logger.

--- src/main/java/com/commercetools/sync/Main.java
+++ src/main/java/com/commercetools/sync/Main.java
@@ -1,13 +1,19 @@
 package com.commercetools.sync;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+
     /**
      * Application entry point.
      *
      * @param args all args
      */
+    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+
     public static void main(final String[] args) {
-        System.out.println("Hello Project Sync!");
+        LOGGER.info("Hello Project Sync!");
     }
 }
