73b37022fa7fb71a07f5da4b2bd5347a8d022cb9
Hesham Massoud <hesham.massoud@commercetools.de>
2018-12-13 11:19:42 +0100

    #5: Apply Spotless.

--- src/main/java/com/commercetools/sync/Main.java
+++ src/main/java/com/commercetools/sync/Main.java
@@ -5,14 +5,14 @@ import org.slf4j.LoggerFactory;
 
 public class Main {
 
-    /**
-     * Application entry point.
-     *
-     * @param args all args
-     */
-    private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+  /**
+   * Application entry point.
+   *
+   * @param args all args
+   */
+  private static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
-    public static void main(final String[] args) {
-        LOGGER.info("Hello Project Sync!");
-    }
+  public static void main(final String[] args) {
+    LOGGER.info("Hello Project Sync!");
+  }
 }
