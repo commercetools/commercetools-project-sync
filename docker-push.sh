20b793ff3bf1db7c976fdb9d617f8f5da785df30
Hesham Massoud <hesham.massoud@commercetools.de>
2019-01-11 16:17:43 +0100

    Fix if conditions.

--- docker-push.sh
+++ docker-push.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
-if test $TRAVIS_BRANCH = "master"
+if [[ $TRAVIS_BRANCH = "master" ]]
 then
     ./gradlew dockerPushImage -Dbuild.version=latest --daemon || travis_terminate 1
 fi
-if test -z $TRAVIS_TAG
+if ! [[ -z $TRAVIS_TAG ]]
 then
     ./gradlew dockerPushImage -Dbuild.version=$TRAVIS_TAG --daemon || travis_terminate 1
 fi
