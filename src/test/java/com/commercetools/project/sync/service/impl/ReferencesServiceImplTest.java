8bcf684544cde6fedf018d17e9c158f054497c02
Hesham Massoud <hesham.massoud@commercetools.de>
2019-10-01 12:27:02 +0200

    #38: Fix assertions.

--- src/test/java/com/commercetools/project/sync/service/impl/ReferencesServiceImplTest.java
+++ src/test/java/com/commercetools/project/sync/service/impl/ReferencesServiceImplTest.java
@@ -1,5 +1,23 @@
 package com.commercetools.project.sync.service.impl;
 
+import com.commercetools.project.sync.model.request.CombinedResourceKeysRequest;
+import com.commercetools.project.sync.model.response.CombinedResult;
+import com.commercetools.project.sync.model.response.ReferenceIdKey;
+import com.commercetools.project.sync.model.response.ResultingResourcesContainer;
+import com.commercetools.project.sync.service.ReferencesService;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import io.sphere.sdk.client.SphereApiConfig;
+import io.sphere.sdk.client.SphereClient;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import uk.org.lidalia.slf4jtest.TestLogger;
+import uk.org.lidalia.slf4jtest.TestLoggerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+
 import static io.sphere.sdk.utils.SphereInternalUtils.asSet;
 import static java.util.Collections.emptyMap;
 import static java.util.Collections.emptySet;
@@ -10,24 +28,6 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-
-import com.commercetools.project.sync.model.request.CombinedResourceKeysRequest;
-import com.commercetools.project.sync.model.response.CombinedResult;
-import com.commercetools.project.sync.model.response.ReferenceIdKey;
-import com.commercetools.project.sync.model.response.ResultingResourcesContainer;
-import com.commercetools.project.sync.service.ReferencesService;
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import io.sphere.sdk.client.SphereApiConfig;
-import io.sphere.sdk.client.SphereClient;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionStage;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
-import uk.org.lidalia.slf4jtest.LoggingEvent;
-import uk.org.lidalia.slf4jtest.TestLogger;
-import uk.org.lidalia.slf4jtest.TestLoggerFactory;
 
 class ReferencesServiceImplTest {
 
@@ -83,8 +83,7 @@ class ReferencesServiceImplTest {
   @SuppressFBWarnings(
       "NP_NONNULL_PARAM_VIOLATION") // https://github.com/findbugsproject/findbugs/issues/79
   @Test
-  void
-      getIdToKeys_WithNullResponse_ShouldReturnCurrentCacheWithoutCachingNewIdsAndLogNonExistentReferences() {
+  void getIdToKeys_WithNullResponse_ShouldReturnCurrentCacheWithoutCachingNewIds() {
     // preparation
     final SphereClient ctpClient = mock(SphereClient.class);
     when(ctpClient.getConfig()).thenReturn(SphereApiConfig.of("test-project"));
@@ -100,23 +99,7 @@ class ReferencesServiceImplTest {
     // assertion
     assertThat(idToKeysStage).isCompletedWithValue(new HashMap<>());
     verify(ctpClient, times(1)).execute(any(CombinedResourceKeysRequest.class));
-    assertThat(testLogger.getAllLoggingEvents())
-        .containsExactlyInAnyOrder(
-            LoggingEvent.error(
-                "Some attribute references point to either a non-existent product (or one with a blank"
-                    + " key) on the source project 'test-project'. These are the reference ids: ('productId')."
-                    + " Please make sure they are existing in the source project and have non-blank (i.e. non-null and"
-                    + " non-empty) keys."),
-            LoggingEvent.error(
-                "Some attribute references point to either a non-existent category (or one with a blank"
-                    + " key) on the source project 'test-project'. These are the reference ids: ('categoryId')."
-                    + " Please make sure they are existing in the source project and have non-blank (i.e. non-null and"
-                    + " non-empty) keys."),
-            LoggingEvent.error(
-                "Some attribute references point to either a non-existent product-type (or one with a blank"
-                    + " key) on the source project 'test-project'. These are the reference ids: ('productTypeId')."
-                    + " Please make sure they are existing in the source project and have non-blank (i.e. non-null and"
-                    + " non-empty) keys."));
+    assertThat(testLogger.getAllLoggingEvents()).isEmpty();
   }
 
   @Test
@@ -183,7 +166,7 @@ class ReferencesServiceImplTest {
   }
 
   @Test
-  void getIdToKeys_WithSomeBlankKeysAndNonExistentIds_ShouldCacheNonBlankKeysAndLogErrors() {
+  void getIdToKeys_WithSomeBlankKeysAndNonExistentIds_ShouldCacheNonBlankKeys() {
     // preparation
     final SphereClient ctpClient = mock(SphereClient.class);
     when(ctpClient.getConfig()).thenReturn(SphereApiConfig.of("test-project"));
@@ -211,70 +194,6 @@ class ReferencesServiceImplTest {
     expectedCache.put("productId", "productKey");
     assertThat(idToKeysStage).isCompletedWithValue(expectedCache);
     verify(ctpClient, times(1)).execute(any(CombinedResourceKeysRequest.class));
-    assertThat(testLogger.getAllLoggingEvents())
-        .containsExactlyInAnyOrder(
-            LoggingEvent.error(
-                "Some attribute references point to either a non-existent product (or one with a blank"
-                    + " key) on the source project 'test-project'. These are the reference ids: ('productId2', 'productId3')."
-                    + " Please make sure they are existing in the source project and have non-blank (i.e. non-null and"
-                    + " non-empty) keys."),
-            LoggingEvent.error(
-                "Some attribute references point to either a non-existent category (or one with a blank"
-                    + " key) on the source project 'test-project'. These are the reference ids: ('categoryId')."
-                    + " Please make sure they are existing in the source project and have non-blank (i.e. non-null and"
-                    + " non-empty) keys."),
-            LoggingEvent.error(
-                "Some attribute references point to either a non-existent product-type (or one with a blank"
-                    + " key) on the source project 'test-project'. These are the reference ids: ('productTypeId')."
-                    + " Please make sure they are existing in the source project and have non-blank (i.e. non-null and"
-                    + " non-empty) keys."));
-  }
-
-  @Test
-  void getIdToKeys_WithSomeNonExistent_ShouldLogErrorsAndReturnExistingCacheWithExistingIds() {
-    // preparation
-    final SphereClient ctpClient = mock(SphereClient.class);
-    when(ctpClient.getConfig()).thenReturn(SphereApiConfig.of("test-project"));
-    final CombinedResult mockResult =
-        new CombinedResult(
-            new ResultingResourcesContainer(asSet(new ReferenceIdKey("productId", "productKey"))),
-            new ResultingResourcesContainer(asSet(new ReferenceIdKey("categoryId", "categoryKey"))),
-            new ResultingResourcesContainer(
-                asSet(new ReferenceIdKey("productTypeId", "productTypeKey"))));
-    when(ctpClient.execute(any(CombinedResourceKeysRequest.class)))
-        .thenReturn(CompletableFuture.completedFuture(mockResult));
-    final ReferencesService referencesService = new ReferencesServiceImpl(ctpClient);
-
-    // test
-    final CompletionStage<Map<String, String>> idToKeysStage =
-        referencesService.getIdToKeys(
-            asSet("productId", "productId1"),
-            asSet("categoryId", "categoryId1"),
-            asSet("productTypeId", "productTypeId1"));
-
-    // assertion
-    final HashMap<String, String> expectedCache = new HashMap<>();
-    expectedCache.put("productId", "productKey");
-    expectedCache.put("categoryId", "categoryKey");
-    expectedCache.put("productTypeId", "productTypeKey");
-    assertThat(idToKeysStage).isCompletedWithValue(expectedCache);
-    verify(ctpClient, times(1)).execute(any(CombinedResourceKeysRequest.class));
-    assertThat(testLogger.getAllLoggingEvents())
-        .containsExactlyInAnyOrder(
-            LoggingEvent.error(
-                "Some attribute references point to either a non-existent product (or one with a blank"
-                    + " key) on the source project 'test-project'. These are the reference ids: ('productId1')."
-                    + " Please make sure they are existing in the source project and have non-blank (i.e. non-null and"
-                    + " non-empty) keys."),
-            LoggingEvent.error(
-                "Some attribute references point to either a non-existent category (or one with a blank"
-                    + " key) on the source project 'test-project'. These are the reference ids: ('categoryId1')."
-                    + " Please make sure they are existing in the source project and have non-blank (i.e. non-null and"
-                    + " non-empty) keys."),
-            LoggingEvent.error(
-                "Some attribute references point to either a non-existent product-type (or one with a blank"
-                    + " key) on the source project 'test-project'. These are the reference ids: ('productTypeId1')."
-                    + " Please make sure they are existing in the source project and have non-blank (i.e. non-null and"
-                    + " non-empty) keys."));
+    assertThat(testLogger.getAllLoggingEvents()).isEmpty();
   }
 }
