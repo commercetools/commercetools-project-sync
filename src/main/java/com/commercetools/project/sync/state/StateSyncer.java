65cd0a44c905e34fd64eeb5aa62d0b7a4e320bf8
Lam Tran <lam.tran@commercetools.de>
2020-08-05 13:43:08 +0200

    #127 format files

--- src/main/java/com/commercetools/project/sync/state/StateSyncer.java
+++ src/main/java/com/commercetools/project/sync/state/StateSyncer.java
@@ -13,55 +13,53 @@ import io.sphere.sdk.states.State;
 import io.sphere.sdk.states.StateDraft;
 import io.sphere.sdk.states.expansion.StateExpansionModel;
 import io.sphere.sdk.states.queries.StateQuery;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import javax.annotation.Nonnull;
 import java.time.Clock;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
+import javax.annotation.Nonnull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class StateSyncer
-        extends Syncer<
-        State,
-        StateDraft,
-        StateSyncStatistics,
-        StateSyncOptions,
-        StateQuery,
-        StateSync> {
+    extends Syncer<
+        State, StateDraft, StateSyncStatistics, StateSyncOptions, StateQuery, StateSync> {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(StateSyncer.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(StateSyncer.class);
 
-    private StateSyncer(@Nonnull StateSync sync,
-                        @Nonnull SphereClient sourceClient,
-                        @Nonnull SphereClient targetClient,
-                        @Nonnull CustomObjectService customObjectService,
-                        @Nonnull Clock clock) {
-        super(sync, sourceClient, targetClient, customObjectService, clock);
-    }
+  private StateSyncer(
+      @Nonnull StateSync sync,
+      @Nonnull SphereClient sourceClient,
+      @Nonnull SphereClient targetClient,
+      @Nonnull CustomObjectService customObjectService,
+      @Nonnull Clock clock) {
+    super(sync, sourceClient, targetClient, customObjectService, clock);
+  }
 
-    public static StateSyncer of(@Nonnull final SphereClient sourceClient,
-                                 @Nonnull final SphereClient targetClient,
-                                 @Nonnull final Clock clock) {
-        StateSyncOptions syncOptions = StateSyncOptionsBuilder.of(targetClient)
-                                                              .errorCallback(LOGGER::error)
-                                                              .warningCallback(LOGGER::error)
-                                                              .build();
-        StateSync stateSync = new StateSync(syncOptions);
-        CustomObjectService customObjectService = new CustomObjectServiceImpl(targetClient);
-        return new StateSyncer(stateSync, sourceClient, targetClient, customObjectService, clock);
-    }
+  public static StateSyncer of(
+      @Nonnull final SphereClient sourceClient,
+      @Nonnull final SphereClient targetClient,
+      @Nonnull final Clock clock) {
+    StateSyncOptions syncOptions =
+        StateSyncOptionsBuilder.of(targetClient)
+            .errorCallback(LOGGER::error)
+            .warningCallback(LOGGER::error)
+            .build();
+    StateSync stateSync = new StateSync(syncOptions);
+    CustomObjectService customObjectService = new CustomObjectServiceImpl(targetClient);
+    return new StateSyncer(stateSync, sourceClient, targetClient, customObjectService, clock);
+  }
 
-    @Nonnull
-    @Override
-    protected CompletionStage<List<StateDraft>> transform(@Nonnull List<State> states) {
-        return CompletableFuture.completedFuture(StateTransitionReferenceReplacementUtils.replaceStateTransitionIdsWithKeys(states));
-    }
+  @Nonnull
+  @Override
+  protected CompletionStage<List<StateDraft>> transform(@Nonnull List<State> states) {
+    return CompletableFuture.completedFuture(
+        StateTransitionReferenceReplacementUtils.replaceStateTransitionIdsWithKeys(states));
+  }
 
-    @Nonnull
-    @Override
-    protected StateQuery getQuery() {
-        return StateQuery.of().withExpansionPaths(StateExpansionModel::transitions);
-    }
+  @Nonnull
+  @Override
+  protected StateQuery getQuery() {
+    return StateQuery.of().withExpansionPaths(StateExpansionModel::transitions);
+  }
 }
