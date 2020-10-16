e567fd0abb85ad1783ffdfd70807e45e168c8c45
Lam Tran <lam.tran@commercetools.de>
2020-10-16 14:39:08 +0200

    #182 Update src/main/java/com/commercetools/project/sync/customer/CustomerSyncer.java
    Co-authored-by: Jude Niroshan <jude.niroshan11@gmail.com>

--- src/main/java/com/commercetools/project/sync/customer/CustomerSyncer.java
+++ src/main/java/com/commercetools/project/sync/customer/CustomerSyncer.java
@@ -75,7 +75,7 @@ public final class CustomerSyncer
             .errorCallback(logErrorCallback)
             .warningCallback(logWarningCallback)
             .build();
-    CustomerSync customerSync = new CustomerSync(customerSyncOptions);
+    final CustomerSync customerSync = new CustomerSync(customerSyncOptions);
     final CustomObjectService customObjectService = new CustomObjectServiceImpl(targetClient);
     return new CustomerSyncer(customerSync, sourceClient, targetClient, customObjectService, clock);
   }
