cfe3892fada1849966abc46f743e44c0365146cf
praveenkumarct <praveen.kumar@commercetools.com>
2020-11-18 14:56:07 +0100

    209 - Add Error Callback tests for the Syncers, Warning callbacks are not implemented for this Syncers.

--- src/test/java/com/commercetools/project/sync/producttype/ProductTypeSyncerTest.java
+++ src/test/java/com/commercetools/project/sync/producttype/ProductTypeSyncerTest.java
@@ -5,20 +5,38 @@ import static io.sphere.sdk.json.SphereJsonUtils.readObjectFromResource;
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import com.commercetools.sync.producttypes.ProductTypeSync;
 import com.commercetools.sync.producttypes.utils.ProductTypeReferenceResolutionUtils;
+import io.sphere.sdk.client.SphereApiConfig;
 import io.sphere.sdk.client.SphereClient;
 import io.sphere.sdk.producttypes.ProductType;
 import io.sphere.sdk.producttypes.ProductTypeDraft;
 import io.sphere.sdk.producttypes.ProductTypeDraftBuilder;
 import io.sphere.sdk.producttypes.queries.ProductTypeQuery;
+import io.sphere.sdk.queries.PagedQueryResult;
+import java.time.Clock;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import uk.org.lidalia.slf4jtest.LoggingEvent;
+import uk.org.lidalia.slf4jtest.TestLogger;
+import uk.org.lidalia.slf4jtest.TestLoggerFactory;
 
 class ProductTypeSyncerTest {
+  private final TestLogger syncerTestLogger =
+      TestLoggerFactory.getTestLogger(ProductTypeSyncer.class);
+
+  @BeforeEach
+  void setup() {
+    syncerTestLogger.clearAll();
+  }
+
   @Test
   void of_ShouldCreateProductTypeSyncerInstance() {
     // test
@@ -66,5 +84,36 @@ class ProductTypeSyncerTest {
 
     // assertion
     assertThat(query).isEqualTo(ProductTypeReferenceResolutionUtils.buildProductTypeQuery(1));
+  }
+
+  @Test
+  void syncWithError_ShouldCallErrorCallback() {
+    // preparation
+    final SphereClient sourceClient = mock(SphereClient.class);
+    final SphereClient targetClient = mock(SphereClient.class);
+    when(sourceClient.getConfig()).thenReturn(SphereApiConfig.of("source-project"));
+    when(targetClient.getConfig()).thenReturn(SphereApiConfig.of("target-project"));
+
+    final List<ProductType> productTypePage =
+        asList(readObjectFromResource("product-type-without-key.json", ProductType.class));
+
+    final PagedQueryResult<ProductType> pagedQueryResult = mock(PagedQueryResult.class);
+    when(pagedQueryResult.getResults()).thenReturn(productTypePage);
+    when(sourceClient.execute(any(ProductTypeQuery.class)))
+        .thenReturn(CompletableFuture.completedFuture(pagedQueryResult));
+
+    // test
+    final ProductTypeSyncer productTypeSyncer =
+        ProductTypeSyncer.of(sourceClient, targetClient, mock(Clock.class));
+    productTypeSyncer.sync(null, true).toCompletableFuture().join();
+
+    // assertion
+    final LoggingEvent errorLog = syncerTestLogger.getAllLoggingEvents().get(0);
+    assertThat(errorLog.getMessage())
+        .isEqualTo(
+            "Error when trying to sync product type. Existing key: <<not present>>. Update actions: []");
+    assertThat(errorLog.getThrowable().get().getMessage())
+        .isEqualTo(
+            "ProductTypeDraft with name: main doesn't have a key. Please make sure all productType drafts have keys.");
   }
 }
--- src/test/java/com/commercetools/project/sync/taxcategory/TaxCategorySyncerTest.java
+++ src/test/java/com/commercetools/project/sync/taxcategory/TaxCategorySyncerTest.java
@@ -5,22 +5,40 @@ import static io.sphere.sdk.json.SphereJsonUtils.readObjectFromResource;
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.toList;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import com.commercetools.sync.taxcategories.TaxCategorySync;
+import io.sphere.sdk.client.SphereApiConfig;
 import io.sphere.sdk.client.SphereClient;
+import io.sphere.sdk.queries.PagedQueryResult;
 import io.sphere.sdk.taxcategories.TaxCategory;
 import io.sphere.sdk.taxcategories.TaxCategoryDraft;
 import io.sphere.sdk.taxcategories.TaxCategoryDraftBuilder;
 import io.sphere.sdk.taxcategories.TaxRateDraft;
 import io.sphere.sdk.taxcategories.TaxRateDraftBuilder;
 import io.sphere.sdk.taxcategories.queries.TaxCategoryQuery;
+import java.time.Clock;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
 import java.util.stream.Collectors;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import uk.org.lidalia.slf4jtest.LoggingEvent;
+import uk.org.lidalia.slf4jtest.TestLogger;
+import uk.org.lidalia.slf4jtest.TestLoggerFactory;
 
 class TaxCategorySyncerTest {
+  private final TestLogger syncerTestLogger =
+      TestLoggerFactory.getTestLogger(TaxCategorySyncer.class);
+
+  @BeforeEach
+  void setup() {
+    syncerTestLogger.clearAll();
+  }
+
   @Test
   void of_ShouldCreateTaxCategorySyncerInstance() {
     // test
@@ -79,5 +97,37 @@ class TaxCategorySyncerTest {
 
     // assertion
     assertThat(query).isEqualTo(TaxCategoryQuery.of());
+  }
+
+  @Test
+  void syncWithError_ShouldCallErrorCallback() {
+    // preparation
+    final SphereClient sourceClient = mock(SphereClient.class);
+    final SphereClient targetClient = mock(SphereClient.class);
+    when(sourceClient.getConfig()).thenReturn(SphereApiConfig.of("source-project"));
+    when(targetClient.getConfig()).thenReturn(SphereApiConfig.of("target-project"));
+
+    final List<TaxCategory> taxCategoryPage =
+        asList(readObjectFromResource("tax-category-without-key.json", TaxCategory.class));
+
+    final PagedQueryResult<TaxCategory> pagedQueryResult = mock(PagedQueryResult.class);
+    when(pagedQueryResult.getResults()).thenReturn(taxCategoryPage);
+    when(sourceClient.execute(any(TaxCategoryQuery.class)))
+        .thenReturn(CompletableFuture.completedFuture(pagedQueryResult));
+
+    // test
+    final TaxCategorySyncer taxCategorySyncer =
+        TaxCategorySyncer.of(sourceClient, targetClient, mock(Clock.class));
+    taxCategorySyncer.sync(null, true).toCompletableFuture().join();
+
+    // assertion
+    final LoggingEvent errorLog = syncerTestLogger.getAllLoggingEvents().get(0);
+    assertThat(errorLog.getMessage())
+        .isEqualTo(
+            "Error when trying to sync tax category. Existing key: <<not present>>. Update actions: []");
+    assertThat(errorLog.getThrowable().get().getMessage())
+        .isEqualTo(
+            "TaxCategoryDraft with name: Standard tax category doesn't have a key. "
+                + "Please make sure all tax category drafts have keys.");
   }
 }
