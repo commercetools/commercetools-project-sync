f986a513fcbc82d184f4d4c9672a7af503e7a9d2
salander85 <sarah.lander@commercetools.com>
2020-12-23 08:23:17 +0100

    Fix format

--- src/main/java/com/commercetools/project/sync/util/ClientConfigurationUtils.java
+++ src/main/java/com/commercetools/project/sync/util/ClientConfigurationUtils.java
@@ -4,24 +4,12 @@ import static io.sphere.sdk.http.HttpStatusCode.BAD_GATEWAY_502;
 import static io.sphere.sdk.http.HttpStatusCode.GATEWAY_TIMEOUT_504;
 import static io.sphere.sdk.http.HttpStatusCode.SERVICE_UNAVAILABLE_503;
 
-import io.sphere.sdk.client.BlockingSphereClient;
-import io.sphere.sdk.client.QueueSphereClientDecorator;
-import io.sphere.sdk.client.RetrySphereClientDecorator;
-import io.sphere.sdk.client.SphereAccessTokenSupplier;
 import io.sphere.sdk.client.SphereClient;
 import io.sphere.sdk.client.SphereClientConfig;
 import io.sphere.sdk.client.retry.RetryableSphereClientBuilder;
 import io.sphere.sdk.http.AsyncHttpClientAdapter;
 import io.sphere.sdk.http.HttpClient;
-import io.sphere.sdk.retry.RetryAction;
-import io.sphere.sdk.retry.RetryPredicate;
-import io.sphere.sdk.retry.RetryRule;
-import java.time.Duration;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
-import java.util.concurrent.TimeUnit;
 import javax.annotation.Nonnull;
 import org.asynchttpclient.AsyncHttpClient;
 import org.asynchttpclient.DefaultAsyncHttpClient;
@@ -40,15 +28,14 @@ public final class ClientConfigurationUtils {
 
     final HttpClient httpClient = getHttpClient();
     return RetryableSphereClientBuilder.of(clientConfig, httpClient)
-                                                     .withStatusCodesToRetry(Arrays.asList(BAD_GATEWAY_502,
-                                                         SERVICE_UNAVAILABLE_503,
-                                                         GATEWAY_TIMEOUT_504))
-                                                     .build();
+        .withStatusCodesToRetry(
+            Arrays.asList(BAD_GATEWAY_502, SERVICE_UNAVAILABLE_503, GATEWAY_TIMEOUT_504))
+        .build();
   }
 
   /**
-   * Gets an asynchronous {@link HttpClient} to be used by the {@link SphereClient}. Client
-   * is created during first invocation and then cached.
+   * Gets an asynchronous {@link HttpClient} to be used by the {@link SphereClient}. Client is
+   * created during first invocation and then cached.
    *
    * @return {@link HttpClient}
    */
--- src/test/java/com/commercetools/project/sync/util/ClientConfigurationUtilsTest.java
+++ src/test/java/com/commercetools/project/sync/util/ClientConfigurationUtilsTest.java
@@ -1,22 +1,19 @@
 package com.commercetools.project.sync.util;
 
-import io.sphere.sdk.client.BlockingSphereClient;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import io.sphere.sdk.client.SphereClient;
 import io.sphere.sdk.client.SphereClientConfig;
 import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.TimeUnit;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 class ClientConfigurationUtilsTest {
 
-    @Test
-    void createClient_WithConfig_ReturnsSphereClient() {
-        final SphereClientConfig clientConfig =
-                SphereClientConfig.of("project-key", "client-id", "client-secret");
-        final SphereClient sphereClient = ClientConfigurationUtils.createClient(clientConfig);
+  @Test
+  void createClient_WithConfig_ReturnsSphereClient() {
+    final SphereClientConfig clientConfig =
+        SphereClientConfig.of("project-key", "client-id", "client-secret");
+    final SphereClient sphereClient = ClientConfigurationUtils.createClient(clientConfig);
 
-        assertThat(sphereClient.getConfig().getProjectKey()).isEqualTo("project-key");
-    }
+    assertThat(sphereClient.getConfig().getProjectKey()).isEqualTo("project-key");
+  }
 }
