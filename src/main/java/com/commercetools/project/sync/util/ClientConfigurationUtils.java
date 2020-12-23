c61c772e2c89d7ae2e2e27542bc3374d8327535a
salander85 <sarah.lander@commercetools.com>
2020-12-23 10:14:33 +0100

    Use default status codes to retry and DefaultAsyncHttpClient with default timeout

--- src/main/java/com/commercetools/project/sync/util/ClientConfigurationUtils.java
+++ src/main/java/com/commercetools/project/sync/util/ClientConfigurationUtils.java
@@ -1,25 +1,18 @@
 package com.commercetools.project.sync.util;
 
-import static io.sphere.sdk.http.HttpStatusCode.BAD_GATEWAY_502;
-import static io.sphere.sdk.http.HttpStatusCode.GATEWAY_TIMEOUT_504;
-import static io.sphere.sdk.http.HttpStatusCode.SERVICE_UNAVAILABLE_503;
-
 import io.sphere.sdk.client.SphereClient;
 import io.sphere.sdk.client.SphereClientConfig;
 import io.sphere.sdk.client.retry.RetryableSphereClientBuilder;
 import io.sphere.sdk.http.AsyncHttpClientAdapter;
 import io.sphere.sdk.http.HttpClient;
-import java.util.Arrays;
 import javax.annotation.Nonnull;
 import org.asynchttpclient.AsyncHttpClient;
 import org.asynchttpclient.DefaultAsyncHttpClient;
 import org.asynchttpclient.DefaultAsyncHttpClientConfig;
 
 public final class ClientConfigurationUtils {
-  private static final long DEFAULT_TIMEOUT = 30000;
-
   /**
-   * Creates a {@link SphereClient} with a default {@code timeout} value of 30 seconds.
+   * Creates a {@link SphereClient} with a default {@code timeout} value of 60 seconds.
    *
    * @param clientConfig the client configuration for the client.
    * @return the instantiated {@link SphereClient}.
@@ -27,24 +20,18 @@ public final class ClientConfigurationUtils {
   public static SphereClient createClient(@Nonnull final SphereClientConfig clientConfig) {
 
     final HttpClient httpClient = getHttpClient();
-    return RetryableSphereClientBuilder.of(clientConfig, httpClient)
-        .withStatusCodesToRetry(
-            Arrays.asList(BAD_GATEWAY_502, SERVICE_UNAVAILABLE_503, GATEWAY_TIMEOUT_504))
-        .build();
+    return RetryableSphereClientBuilder.of(clientConfig, httpClient).build();
   }
 
   /**
-   * Gets an asynchronous {@link HttpClient} to be used by the {@link SphereClient}. Client is
-   * created during first invocation and then cached.
+   * Gets an asynchronous {@link HttpClient} of `asynchttpclient` library, to be used by as an
+   * underlying http client for the {@link SphereClient}.
    *
-   * @return {@link HttpClient}
+   * @return an asynchronous {@link HttpClient}
    */
-  public static HttpClient getHttpClient() {
+  private static HttpClient getHttpClient() {
     final AsyncHttpClient asyncHttpClient =
-        new DefaultAsyncHttpClient(
-            new DefaultAsyncHttpClientConfig.Builder()
-                .setHandshakeTimeout((int) DEFAULT_TIMEOUT)
-                .build());
+        new DefaultAsyncHttpClient(new DefaultAsyncHttpClientConfig.Builder().build());
     return AsyncHttpClientAdapter.of(asyncHttpClient);
   }
 
