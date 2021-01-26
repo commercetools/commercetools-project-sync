75c40b5754af6998711578986c1e215c6df77595
Ahmet Öz <bilmuhahmet@gmail.com>
2021-01-26 14:01:36 +0100

    Delete docker-push.sh

--- docker-push.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-if [[ $TRAVIS_BRANCH = "master" ]]
-then
-    ./gradlew dockerPushImage -Dbuild.version=latest --daemon || travis_terminate 1
-fi
-if ! [[ -z $TRAVIS_TAG ]]
-then
-    ./gradlew dockerPushImage -Dbuild.version=$TRAVIS_TAG --daemon || travis_terminate 1
-fi
