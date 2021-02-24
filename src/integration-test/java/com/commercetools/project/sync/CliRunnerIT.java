65b467186de691a02f3d82b7c63058189e527bb7
praveenkumarct <praveen.kumar@commercetools.com>
2021-02-24 23:36:13 +0100

    - Adding Integration Tests

--- src/integration-test/java/com/commercetools/project/sync/CliRunnerIT.java
+++ src/integration-test/java/com/commercetools/project/sync/CliRunnerIT.java
@@ -1061,6 +1061,40 @@ class CliRunnerIT {
     }
   }
 
+  @Test
+  void run_WithSyncAsArgumentWithAllArgAsFullSyncAndWithCustomQueryAndFetchSizeForProducts_ShouldExecuteAllSyncers() {
+    // preparation
+    try (final SphereClient targetClient = createClient(CTP_TARGET_CLIENT_CONFIG)) {
+      try (final SphereClient sourceClient = createClient(CTP_SOURCE_CLIENT_CONFIG)) {
+
+        final SyncerFactory syncerFactory =
+            SyncerFactory.of(() -> sourceClient, () -> targetClient, Clock.systemDefaultZone());
+
+        final Long fetchSize = 100L;
+        final String customQuery =
+            "\"masterData(published=true) AND masterData(staged(masterVariant(key= \\\"foo\\\")))\"";
+        final String productSyncConfigValue =
+            "{\"fetchSize\": " + fetchSize + ", \"customQuery\": " + customQuery + "}";
+
+        // test
+        CliRunner.of().run(new String[] {"-s", "all", "-r", "runnerName", "-f",
+            "-productSyncConfig", productSyncConfigValue}, syncerFactory);
+      }
+    }
+
+    // create clients again (for assertions and cleanup), since the run method closes the clients
+    // after execution is done.
+    try (final SphereClient postTargetClient = createClient(CTP_TARGET_CLIENT_CONFIG)) {
+      // assertions
+      assertAllSyncersLoggingEvents(syncerTestLogger, cliRunnerTestLogger, 1);
+
+      assertAllResourcesAreSyncedToTarget(postTargetClient);
+      assertCurrentCtpTimestampGeneratorDoesntExist(
+          postTargetClient, "runnerName", "ProductTypeSync");
+      assertNoProjectSyncCustomObjectExists(postTargetClient);
+    }
+  }
+
   private void assertNoProjectSyncCustomObjectExists(@Nonnull final SphereClient targetClient) {
     final CustomObjectQuery<LastSyncCustomObject> lastSyncQuery =
         CustomObjectQuery.of(LastSyncCustomObject.class)
