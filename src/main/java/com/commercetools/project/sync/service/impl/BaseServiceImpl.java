93e37814adc53f29eb5f4168266b017489cc2ada
praveenkumarct <praveen.kumar@commercetools.com>
2021-02-25 22:55:01 +0100

    - Make cache static, to use it as generic for all resources.

--- src/main/java/com/commercetools/project/sync/service/impl/BaseServiceImpl.java
+++ src/main/java/com/commercetools/project/sync/service/impl/BaseServiceImpl.java
@@ -20,22 +20,20 @@ import javax.annotation.Nonnull;
 
 public class BaseServiceImpl {
   private final SphereClient ctpClient;
-  protected final Cache<String, String> referenceIdToKeyCache;
-
-  // TODO: This can be static instead of an instance-based cache.
-  // Assume the CategoryReferenceTransformServiceImpl has cached almost all categories ids to keys,
-  // and then ProductReferenceTransformServiceImpl will be initialized with a new cache which will
-  // fetch all referenced category keys again.
-  private long cacheSize = 10_000;
+  private static long cacheSize = 10_000;
+  protected static final Cache<String, String> referenceIdToKeyCache = initializeCache();
 
   protected BaseServiceImpl(@Nonnull final SphereClient ctpClient) {
     this.ctpClient = ctpClient;
-    this.referenceIdToKeyCache =
-        Caffeine.newBuilder().maximumSize(cacheSize).executor(Runnable::run).build();
   }
 
   protected SphereClient getCtpClient() {
     return ctpClient;
+  }
+
+  @Nonnull
+  private static Cache<String, String> initializeCache() {
+    return Caffeine.newBuilder().maximumSize(cacheSize).executor(Runnable::run).build();
   }
 
   protected <T extends WithKey> CompletionStage<List<T>> fetchAndFillReferenceIdToKeyCache(
