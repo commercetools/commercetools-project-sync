f06703fc4ecaddf75a8393ed927edfc60f41edea
praveenkumarct <praveen.kumar@commercetools.com>
2021-09-15 15:58:11 +0200

    revert sync-java version to check the flaky test failure.

--- src/test/java/com/commercetools/project/sync/state/StateSyncerTest.java
+++ src/test/java/com/commercetools/project/sync/state/StateSyncerTest.java
@@ -82,6 +82,7 @@ class StateSyncerTest {
             .description(LocalizedString.ofEnglish("State 1"))
             .name(LocalizedString.ofEnglish("State 1"))
             .initial(true)
+            .transitions(Collections.emptySet())
             .build();
     final StateDraft draft2 =
         StateDraftBuilder.of("State 2", StateType.LINE_ITEM_STATE)
