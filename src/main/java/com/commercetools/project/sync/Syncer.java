8c04234d682c1bd4d2a3b992d37dd988966aa17e
lojzatran <lam.tran@commercetools.com>
2023-11-03 19:01:44 +0100

    DEVX-245 correct code

--- src/main/java/com/commercetools/project/sync/Syncer.java
+++ src/main/java/com/commercetools/project/sync/Syncer.java
@@ -208,10 +208,11 @@ public abstract class Syncer<
   @Nonnull
   private PagedQueryT getQueryWithTimeBoundedPredicate(
       @Nonnull final ZonedDateTime lowerBound, @Nonnull final ZonedDateTime upperBound) {
-
-    final String queryPredicate =
-        format("lastModifiedAt >= \"%s\" AND lastModifiedAt <= \"%s\"", lowerBound, upperBound);
-    return (PagedQueryT) getQuery().withWhere(queryPredicate);
+    return (PagedQueryT)
+        getQuery()
+            .withWhere("lastModifiedAt >= :lower AND lastModifiedAt <= :upper")
+            .withPredicateVar("lower", lowerBound)
+            .withPredicateVar("upper", upperBound);
   }
 
   @Nonnull
